28a801d43a1695cc1aafaf3bd0dee91756e0d755
Sugimaru <sugimaruru@gmail.com>
2022-01-10 16:34:48 +0900

    リファクタリング（無駄な改行消しなど）

--- opensalesbarweb/src/views/News.vue
+++ opensalesbarweb/src/views/News.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="news">
     <h1>This is a news page</h1>
+    <p>画像直リンクのテスト</p>
+    <img src="https://chie-pctr.c.yimg.jp/dk/iwiz-chie/que-12188593977?w=999&h=999&up=0" alt="察してくれ">
   </div>
 </template>
 
