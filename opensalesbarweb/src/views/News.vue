d783d3453014c7e981e9bedb1cd9c2fa708cbaad
Sugimaru <sugimaruru@gmail.com>
2022-01-16 17:15:36 +0900

    直リンク画像をS3から呼ぶように変更

--- opensalesbarweb/src/views/News.vue
+++ opensalesbarweb/src/views/News.vue
@@ -2,7 +2,7 @@
   <div id="news">
     <h1>This is a news page</h1>
     <p>画像直リンクのテスト</p>
-    <img src="https://chie-pctr.c.yimg.jp/dk/iwiz-chie/que-12188593977?w=999&h=999&up=0" alt="察してくれ">
+    <img src="https://gismarck-s3-001.s3.ap-northeast-1.amazonaws.com/img/testimg.png" alt="テスト画像">
   </div>
 </template>
 
